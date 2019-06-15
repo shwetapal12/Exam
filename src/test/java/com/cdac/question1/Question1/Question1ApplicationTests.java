e2073dce5f66d7bc515082e08c00b00c47a35bc3
group004 <shwetapal9158@gmail.com>
2019-06-15 08:17:43 +0530

    testNG submitted

--- src/test/java/com/cdac/question1/Question1/Question1ApplicationTests.java
+++ src/test/java/com/cdac/question1/Question1/Question1ApplicationTests.java
@@ -1,12 +1,8 @@
 package com.cdac.question1.Question1;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.testng.annotations.Test;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 public class Question1ApplicationTests {
 
 	@Test
